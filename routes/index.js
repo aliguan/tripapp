976e8e946f3089bc66e316f3fc6b4beae1b8fa39
Alice Guan <aliguan726@gmail.com>
2017-06-02 23:30:43 -0400

    Enable search for locations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const passport     = require('passport');
+const ensure       = require('connect-ensure-login');
+const mongoose     = require('mongoose');
+const multer       = require('multer');
+const User         = require('../models/user-model.js');
+const Trip         = require('../models/trip-model.js');
+const flash        = require('connect-flash');
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.post('/', (req, res, next) => {
+    const userSearchLoc = req.body.searchLocation;
+
+    Trip.find(
+        { location: userSearchLoc }, (err, tripsFound) => {
+            if(err) { return next(err); }
+            res.render('trips/tripsinlocation.ejs', { trips: tripsFound });
+        });
 });
 
 module.exports = router;
