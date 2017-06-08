ae8a17f529305eb8c1de47f9a59d3b655df3b4d0
Alice Guan <aliguan726@gmail.com>
2017-06-08 00:01:45 -0400

    redeploy

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
         $('.expandtripdiv').on('click', function() {
-            $('.addtrip').toggle();
+            $('.addtrip').animate({
+                height: 'toggle',
+            });
         });
         $('.expandtripdiv').on('click', function() {
             $('.plussign').toggleClass('fa-plus');
@@ -50,55 +52,5 @@ $( document ).ready(function() {
         });
 
     }
-
-        getTrips();
-        jiggle();
+    getTrips();
 });
-
-
-//GOOGLE MAP
-
-    var sol = {
-            lat: 22.286394,
-            lng: 114.149139
-    };
-
-  var geocoder;
-  var map;
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var mapOptions = {
-      zoom: 8,
-      center: sol,
-  };
-    map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: sol
-      });
-  }
-
- 
-function jiggle() {
-    let markers = [];
-    mydest.forEach(function(destination){
-        var address = destination.address;
-        geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
-        console.log(results[0]);
-            map.setCenter(results[0].geometry.location);
-
-            let title = destination.name;
-            let position = results[0].geometry.location;
-
-          var pin = new google.maps.Marker({ position, map, title  });
-          markers.push(pin);
-
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-
-  });
-}
-
-    initialize();
