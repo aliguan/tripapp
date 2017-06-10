2ac503b162a165d8c106e6f269c95027d50658cf
Alice Guan <aliguan726@gmail.com>
2017-06-09 23:33:36 -0400

    why h10

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
